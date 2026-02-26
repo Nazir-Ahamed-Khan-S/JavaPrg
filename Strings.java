b26859cb973b16389f71834dfd256722bda8f052
Nazir-Ahamed-Khan-S <nazeerahamedkhan158@gmail.com>
2026-02-26 16:05:19 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public static void main(String[] args){
+    System.out.println("Enter your name:");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    System.out.println("your name is:"+name);
+}
