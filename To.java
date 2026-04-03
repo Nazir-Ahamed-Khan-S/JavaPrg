a4bdb45865494d200dc55cedc36be8470241aada
Nazir-Ahamed-Khan-S <nazeerahamedkhan158@gmail.com>
2026-04-03 18:55:28 +0530

    Create To.java

---
+++ To.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class to{
+    public static void main(String[] args){
+        int num1 = 60 ;
+        int num2 = 70 ;
+        int result = num1 > num2 ? num1 : num2;
+        System.out.println(result);
+    }
+}
