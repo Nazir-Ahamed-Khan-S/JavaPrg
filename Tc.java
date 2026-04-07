9d49ebf89962d3bdb1e71a0b36b007118afa36de
Nazir-Ahamed-Khan-S <nazeerahamedkhan158@gmail.com>
2026-04-07 16:38:35 +0530

    Update Tc.java

--- Tc.java
+++ Tc.java
@@ -1,12 +1,11 @@
-import java.util.*;
 public class Tc{
     public static void main(String[] args){
-      int radius = 500 ;
-      double newradius = radius;
-      System.out.println(newradius);
-      
-      double diameter = 800.25;
-      int newdiameter = (int)diameter;
-      System.out.println(newdiameter);
+        int radius = 500;
+        double newradius = radius;
+        System.out.println(newradius);
+
+        double diameter = 800.25;
+        int newdiameter = (int)diameter;
+        System.out.println(newdiameter);
     }
 }
