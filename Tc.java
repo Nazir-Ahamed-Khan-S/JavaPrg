fe0a2153630e798371218b139f8b96b7a7ab819d
Nazir-Ahamed-Khan-S <nazeerahamedkhan158@gmail.com>
2026-04-07 16:33:58 +0530

    Create Tc.java

---
+++ Tc.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Tc{
+    public static void main(String[] args){
+      int radius = 500 ;
+      double newradius = radius;
+      System.out.println(newradius);
+      
+      double diameter = 800.25;
+      int newdiameter = (int)diameter;
+      System.out.println(newdiameter);
+    }
+}
