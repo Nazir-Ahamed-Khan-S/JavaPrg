1d669a5093895a78a0ca6e72319dbad1cca95e0c
Nazir-Ahamed-Khan-S <nazeerahamedkhan158@gmail.com>
2026-05-03 19:03:46 +0530

    Update BankAccountApplication.java

--- BankApp/BankAccountApplication.java
+++ BankApp/BankAccountApplication.java
@@ -4,18 +4,20 @@ public class BankAccountApplication {
 
     public static void main(String[] args) {
 
-        BankAccount rbsAccount1 = new BankAccount();
-        rbsAccount1.accountNumber = 101;
-        rbsAccount1.balance = 10000.00;
+        BankAccount rbsAccount1 = new BankAccount(101,10000.00);
+        //rbsAccount1.accountNumber = 101;
+        //rbsAccount1.balance = 10000.00;
+
+        System.out.println(rbsAccount1.getBalance());   
 
         rbsAccount1.deposit_amount(15000.00);
-        rbsAccount1.withdraw_amount(10000.00);
+        rbsAccount1.withdraw_amount(10000.00);          
         rbsAccount1.check_balance();
 
-        BankAccount rbsAccount2 = new BankAccount();
-        rbsAccount2.accountNumber = 201;
-        rbsAccount2.balance = 50000.00;
+        BankAccount rbsAccount2 = new BankAccount(201,5000.00);
+        // rbsAccount2.accountNumber = 201;
+        // rbsAccount2.balance = 50000.00;
 
-        rbsAccount2.withdraw_amount(5000);
+        rbsAccount2.withdraw_amount(5000);             
     }
 }
